2c15fdb1a3bdb9632bbb82e46e89967d8d93e73a
CQUEEN-lpy <79646025+CQUEEN-lpy@users.noreply.github.com>
2021-07-13 09:44:46 +0800

    can train binary CLS task now

--- P&T/model_train.py
+++ P&T/model_train.py
@@ -1,6 +1,7 @@
 '''
 import
 '''
+import keras.losses
 import pandas as pd
 from model.EFF import *
 from tensorflow.keras.callbacks import (
@@ -17,21 +18,22 @@ img_height = 480
 """
 the following train function is for training the CLS model
 Parameters:
-1. save_dir: the checkpoint you want to save, please use the format like '/home/zhang.xinxi/CV/checkpoint/man_top_color/cp-{epoch:04d}.ckpt' 
+1. save_dir: the checkpoint you want to save, please use the format like '/home/zhang.xinxi/CV/checkpoint/man_top_color/cp-{epoch:04d}.ckpt'
    so that the function can save different epochs.
 2. csv_dir: the dir for logging the training history(e.g. acc/loss), end with '.csv'.
-3. data_dir: the dir for the data, we use the tf api to load the data information(including the class information),so the data dir should be 
+3. data_dir: the dir for the data, we use the tf api to load the data information(including the class information),so the data dir should be
    structured(data structure: root path/class_name/images), and we use the root as the data_dir here.
 4. best_path: since the we use the early stop mechanism here, we store the best epoch of the model to the best_path
 5. epochs: the max epochs to train the model
-6. portion: the portion for the training set -> (1-portion) is the the portion for the val set 
+6. portion: the portion for the training set -> (1-portion) is the the portion for the val set
 7. log_dir: the class information of the data, (e.g. '方领','v领','圆领'), end with '.csv'
 8. begin_epoch: if you want to continue training using a certain checkpoint, you can enter the checkpoint here for the model to load
    the checkpoint must be ended with '.ckpt'
-9. distribute_training: whether to use multiple GPU to train 
+9. distribute_training: whether to use multiple GPU to train
+10.binary: whether the CLS task is binary
 """
 
-def train(save_dir, csv_dir, data_dir, best_path, epochs, batch_size, portion,log_dir,begin_epoch='no',distribute_training=False):
+def train(save_dir, csv_dir, data_dir, best_path, epochs, batch_size, portion,log_dir,begin_epoch='no',distribute_training=False,binary=False):
     """
     call backs
     """
@@ -66,12 +68,13 @@ def train(save_dir, csv_dir, data_dir, best_path, epochs, batch_size, portion,lo
     print('the data has been loaded', train_ds.class_names)
 
     # transfer the label to one_hot form
-    def map_func(images, label):
-        one_hot_label = tf.one_hot(label, depth=class_nums)
-        return images, one_hot_label
-
-    train_ds = train_ds.map(map_func, num_parallel_calls=tf.data.AUTOTUNE)
-    val_ds = val_ds.map(map_func, num_parallel_calls=tf.data.AUTOTUNE)
+    if not binary:
+        def map_func(images, label):
+            one_hot_label = tf.one_hot(label, depth=class_nums)
+            return images, one_hot_label
+    
+        train_ds = train_ds.map(map_func, num_parallel_calls=tf.data.AUTOTUNE)
+        val_ds = val_ds.map(map_func, num_parallel_calls=tf.data.AUTOTUNE)
 
     '''
     load the EFF model
@@ -79,16 +82,25 @@ def train(save_dir, csv_dir, data_dir, best_path, epochs, batch_size, portion,lo
     if distribute_training:
         mirrored_strategy = tf.distribute.MirroredStrategy()
         with mirrored_strategy.scope():
-            model = get_EFFmodel(img_height, img_width, class_nums)
+            model = get_EFFmodel(img_height, img_width, class_nums, binary=binary)
             if 'ckpt' in begin_epoch:
                 model.load_weights(begin_epoch)
-            model.compile(optimizer=keras.optimizers.Adam(lr=1e-4), loss=keras.losses.CategoricalCrossentropy(label_smoothing=0.1), metrics=['accuracy'])
+            if binary:
+                model.compile(optimizer=keras.optimizers.Adam(lr=1e-4),
+                              loss=keras.losses.CategoricalCrossentropy(label_smoothing=0.1), metrics=['accuracy'])
+            else:
+                model.compile(optimizer=keras.optimizers.Adam(lr=1e-4),
+                              loss=keras.losses.BinaryCrossentropy(label_smoothing=0.1), metrics=['accuracy'])
     else:
-        model = get_EFFmodel(img_height, img_width, class_nums)
+        model = get_EFFmodel(img_height, img_width, class_nums, binary=binary)
         if 'ckpt' in begin_epoch:
             model.load_weights(begin_epoch)
-        model.compile(optimizer=keras.optimizers.Adam(lr=1e-4),
-                      loss=keras.losses.CategoricalCrossentropy(label_smoothing=0.1), metrics=['accuracy'])
+        if binary:
+            model.compile(optimizer=keras.optimizers.Adam(lr=1e-4),
+                          loss=keras.losses.CategoricalCrossentropy(label_smoothing=0.1), metrics=['accuracy'])
+        else:
+            model.compile(optimizer=keras.optimizers.Adam(lr=1e-4),
+                          loss=keras.losses.BinaryCrossentropy(label_smoothing=0.1), metrics=['accuracy'])
     print(model.summary())
 
     '''
@@ -117,5 +129,3 @@ def train(save_dir, csv_dir, data_dir, best_path, epochs, batch_size, portion,lo
     # save the class information to the log_dir
     names_df = pd.DataFrame(class_names).set_index(0)
     names_df.to_csv(log_dir)
-
-
